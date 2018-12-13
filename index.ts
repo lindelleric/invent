7faf604d7b8e982ce29fd3c810305bf06fbaa4ac
Eric Nilsson <eric.nilsson@infrasightlabs.com>
2018-12-13 11:00:51 +0100

    Working scraper. It only prints to console

---
+++ index.ts
@@ -0,0 +1,28 @@
+
+import * as request from 'request';
+import * as cheerio from 'cheerio';
+
+const r = request.defaults({ jar: true })
+
+r('https://arcamea.com/sv/shop/?add-to-cart=634', () => {
+    console.log('30mm Done');
+
+    r('https://arcamea.com/sv/shop/?add-to-cart=731', () => {
+        console.log('35mm Done');
+
+        r('https://arcamea.com/sv/shop/?add-to-cart=732', () => {
+            console.log('40mm Done');
+
+            r('https://arcamea.com/sv/shop/?add-to-cart=785', () => {
+                console.log('Prov Done');
+
+                r('https://arcamea.com/basket', (error, response, body) => {
+                    const doc = cheerio.load(body);
+                    const items = doc('.cart_item input').map((i, item) => {
+                        console.log('item.attribs', item.attribs['aria-labelledby'], item.attribs['max']);
+                    });
+                });
+            });
+        });
+    });
+});

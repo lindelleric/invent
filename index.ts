bf6d463ec7bc3197d5b4a9680e079d4863f9b91f
Eric Nilsson <eric.nilsson@infrasightlabs.com>
2019-01-02 08:39:22 +0100

    fix path to new basket and selector for new cart-item

--- index.ts
+++ index.ts
@@ -16,11 +16,13 @@ r('https://arcamea.com/sv/shop/?add-to-cart=634', () => {
             r('https://arcamea.com/sv/shop/?add-to-cart=785', () => {
                 console.log('Prov Done');
 
-                r('https://arcamea.com/basket', (error, response, body) => {
+                r('https://arcamea.com/varukorg', (error, response, body) => {
                     const doc = cheerio.load(body);
                     let meh = '';
 
-                    const items = doc('.cart_item input').map((i, item) => {
+                    console.log('------------------------------');
+
+                    const items = doc('.product-quantity input').map((i, item) => {
                         console.log(item.attribs['aria-labelledby'], item.attribs['max']);
                         meh += `${ item.attribs['max'] } `;
                     });
